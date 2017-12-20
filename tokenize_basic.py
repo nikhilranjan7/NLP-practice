1ad6ec61a77584b7ab9c0eca1262bd7ea9749b5b
Nikhil Ranjan <nikhilranjan7@gmail.com>
2017-12-20 17:00:07 +0800

    NLP practice

---
+++ tokenize_basic.py
@@ -0,0 +1,13 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+example_text = "Hello there, how are you doing today? The weather is great and Python is awesome. The sky is pinkish-blue. You should not eat cardboard."
+
+print(sent_tokenize(example_text))
+
+print(word_tokenize(example_text))
+
+for i in word_tokenize(example_text):
+    print(i)
+
+for i in sent_tokenize(example_text):
+    print(i)
